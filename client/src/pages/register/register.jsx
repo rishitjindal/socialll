d4663589a21b3cebdf398139d25abc153dd98cce
rishitjindal <rishitjindal2@gmail.com>
2025-06-27 23:11:45 +0530

    Create register.jsx

---
+++ client/src/pages/register/register.jsx
@@ -0,0 +1,11 @@
+// client/src/pages/register/Register.jsx
+
+import React from "react";
+
+export default function Register() {
+  return (
+    <div>
+      <h2>Register Page</h2>
+    </div>
+  );
+}
